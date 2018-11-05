629de80a8816df463757c1440d66493467543a7a
Agon17 <a.gon.mz@hotmail.com>
2018-11-05 16:39:00 -0600

    Corrección Scanner

--- src/main/java/ahorcado.java
+++ src/main/java/ahorcado.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class ahorcado {
 
     public static void main(String[] args) {
-	//error
+	Scanner s = new Scanner(System.in);
         int vidas = 6;
         String palabra;
         int aciertos = 0;
